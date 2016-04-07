1ff1a928688b005bc3b4bc217ba83edb180b34a9
10igeorge <10izzygeorge@gmail.com>
2016-04-07 14:56:41 -0700

    add shopping cart icon

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'js-stumptown',
+    podModulePrefix: 'js-stumptown/pods',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
     firebase: 'https://js-stumptown.firebaseio.com/',
