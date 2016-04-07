a01add59c50b62227aa4bd742784f05bb1f5cf2e
10igeorge <10izzygeorge@gmail.com>
2016-04-06 20:58:03 -0700

    create seed.json for example models

--- app/models/product.js
+++ app/models/product.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   category: DS.belongsTo('category', {async:true}),
   name: DS.attr(),
   description: DS.attr(),
-  price: DS.attr(),
+  price: DS.attr('number'),
   origin: DS.attr(),
   notes: DS.attr()
 });
